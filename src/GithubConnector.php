a6cac3672bb3cfbefb4446e633d16717b0562ef9
Jordan Partridge <jordan@partridge.rocks>
2025-06-27 21:48:54 -0700

    docs: restore clean docblocks to connector
    - Add professional class and method documentation
    - Remove "REMOVED:" comments that cluttered the code
    - Keep docblocks focused on what methods do, not what was removed

--- src/GithubConnector.php
+++ src/GithubConnector.php
@@ -7,27 +7,44 @@ use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
 use Saloon\Traits\Plugins\AcceptsJson;
 
+/**
+ * GitHub API connector for Saloon HTTP client.
+ */
 class GithubConnector extends Connector implements GithubConnectorInterface
 {
     use AcceptsJson;
 
     protected ?string $token;
 
+    /**
+     * Create a new GitHub connector instance.
+     *
+     * @param string|null $token GitHub personal access token
+     */
     public function __construct(?string $token = null)
     {
         $this->token = $token;
     }
 
+    /**
+     * Get the base URL for the GitHub API.
+     */
     public function resolveBaseUrl(): string
     {
         return 'https://api.github.com';
     }
 
+    /**
+     * Configure default authentication for requests.
+     */
     protected function defaultAuth(): TokenAuthenticator
     {
         return new TokenAuthenticator($this->token);
     }
 
+    /**
+     * Configure default headers for all requests.
+     */
     protected function defaultHeaders(): array
     {
         return [
